2cb71b5cf782243305ecedc2081dc1de31fcb767
NoireMare <109278616+NoireMare@users.noreply.github.com>
2023-01-25 15:50:56 +0200

    App.py and Schemas changed
    Api switched to router, new model names in schemas.

--- app.py
+++ app.py
@@ -1,37 +1,17 @@
-from fastapi import Depends, FastAPI, Request
+from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from starlette.responses import JSONResponse
 
-import schemas
+from routers import passes
 from database.models import Base
-from database.database import engine, SessionLocal
-from sqlalchemy.orm import Session
-from database import crud
+from database.database import engine
 
 
 Base.metadata.create_all(bind=engine)
 
 api = FastAPI()
 
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@api.post("/new_pass/create", status_code=201)
-def submitData(pass_: schemas.Pass, user: schemas.User, level: schemas.Level, image: schemas.Image,
-               coordinate: schemas.Coordinate, db: Session = Depends(get_db)):
-    crud.CreatePass.create_user(db=db, user=user)
-    crud.CreatePass.create_level(db=db, level=level)
-    crud.CreatePass.create_images(db=db, image=image)
-    crud.CreatePass.create_coordinate(db=db, coordinate=coordinate)
-    new_pass = crud.CreatePass.create_pass(db=db, pass_=pass_)
-    return JSONResponse({"status": 200, "message": "New pass is created", "id": new_pass.id})
+api.include_router(passes.router)
 
 
 @api.exception_handler(RequestValidationError)
@@ -42,4 +22,3 @@ async def validation_exception_handler(request, exc):
 @api.exception_handler(Exception)
 async def exception_callback(request: Request, exc: Exception):
     return JSONResponse({"status": 500, "message": str(exc)[:200], "id": "None"}, status_code=500)
-
--- schemas.py
+++ schemas.py
@@ -1,46 +1,46 @@
-from pydantic import BaseModel, validator, constr
+from pydantic import BaseModel, validator, constr, EmailStr
 from datetime import datetime
-from typing import Type, Any
+from typing import Type
 
 
 class BaseException(Exception):
     pass
 
 
-class User(BaseModel):
+class UserScheme(BaseModel):
     fam: str
     name: str
     otc: str = None
     phone: str = None
-    email: str
+    email: EmailStr
 
     @validator('email')
-    def validate_email(cls: Type['User'], value: str) -> str:
+    def validate_email(cls: Type['UserScheme'], value: str) -> str:
         if not all(char in value for char in ['@', '.']):
             raise BaseException('The email is wrong')
         return value
 
     @validator('phone')
-    def validate_phone(cls: Type['User'], value: str) -> str:
+    def validate_phone(cls: Type['UserScheme'], value: str) -> str:
         if not all(digit.isdigit() for digit in value):
             raise BaseException('The phone number is wrong')
         return value
 
 
-class Coordinate(BaseModel):
+class CoordinateScheme(BaseModel):
     latitude: float
     longitude: float
     height: int
 
 
-class Level(BaseModel):
+class LevelScheme(BaseModel):
     winter: constr(max_length=2) = None
     summer: constr(max_length=2) = None
     autumn: constr(max_length=2) = None
     spring: constr(max_length=2) = None
 
 
-class Image(BaseModel):
+class ImageScheme(BaseModel):
     title_1: str = None
     image_1: str
     title_2: str = None
@@ -49,7 +49,7 @@ class Image(BaseModel):
     image_3: str = None
 
 
-class Pass(BaseModel):
+class PassScheme(BaseModel):
     add_time: datetime
     beauty_title: str
     title: str
