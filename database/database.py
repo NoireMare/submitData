f17bacb5e4a27ff5898cee355cd8445919e8006f
NoireMare <109278616+NoireMare@users.noreply.github.com>
2023-01-28 22:29:59 +0200

    Update database.py

--- database/database.py
+++ database/database.py
@@ -4,10 +4,9 @@ from sqlalchemy.orm import sessionmaker
 from config import *
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{FSTR_DB_LOGIN}:{FSTR_DB_PASS}@{FSTR_DB_HOST}:{FSTR_DB_PORT}/postgres"
+SQLALCHEMY_DATABASE_URL = f"postgresql://hjgkvsqcldwomq:c1361c77630e4a9f428fa9eb77df60c8208af12765b098d7556c90692236b0aa@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d2m38cbk39p3lq"
 
-# f"postgresql://hjgkvsqcldwomq:c1361c77630e4a9f428fa9eb77df60c8208af12765b098d7556c90692236b0aa@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d2m38cbk39p3lq"
-
+# f"postgresql://{FSTR_DB_LOGIN}:{FSTR_DB_PASS}@{FSTR_DB_HOST}:{FSTR_DB_PORT}/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
