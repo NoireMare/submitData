70a0308bc43fcdccfac25564679b365f44b88775
NoireMare <109278616+NoireMare@users.noreply.github.com>
2023-02-02 22:04:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ FSTR_DB_PORT = os.getenv('FSTR_DB_PORT')
 FSTR_DB_LOGIN = os.getenv('FSTR_DB_LOGIN')
 FSTR_DB_PASS = os.getenv('FSTR_DB_PASS')
 
-API_URL = 'http://127.0.0.1:8000'
+API_URL = 'https://passes-app.herokuapp.com'
 
